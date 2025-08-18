ac4aab5f293ad3b3e5a857a770a3fec8a7dc2af5
Jude Woolls <jwoolls01@gmail.com>
2025-08-18 19:26:52 +0100

    add the jwt token authentication urls and views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_view(request):
+    """
+    Handle user login.
+    """
+    if request.method == 'POST':
+        # Process login form
+        pass
